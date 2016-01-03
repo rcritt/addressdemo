c161a09437223bf75a7ebc41c267ea5816f136fb
rcritt <rwag@rwag.net>
2016-01-02 21:03:44 -0500

    The routing structure for the demo is in-place.

--- webserver/main.go
+++ webserver/main.go
@@ -7,10 +7,36 @@ import (
 )
 
 func main() {
-	router := mux.NewRouter()
-	fmt.Println("P=" + http.Dir("static"))
+	router := mux.NewRouter().StrictSlash(true)
+
+	// ****************
+	// STATIC FILES
+	// ****************
 	fs := http.FileServer(http.Dir("static"))
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+
+	// ****************
+	// REST APIs
+	// ****************
+	restAPIList := func(w http.ResponseWriter, r *http.Request) {
+
+	}
+
+	// REST-API: /list
+	router.HandleFunc("/api/list", restAPIList)
+
+	// REST-API: /create
+	//           Create takes a JSON create request that was posted.
+	//	router.HandlerFunc("/api/create/{
+	// TODO: https://github.com/corylanou/tns-restful-json-api/tree/master/v9
+	//       Nice logging and clean organization...
+	restAPICreate := func(w http.ResponseWriter, r *http.Request) {
+		// Read in the posted JSON request.
+		// Generate a map from that.
+		// Use the information to create a new address.
+
+	}
+	router.HandleFunc("/api/create", restAPICreate).Methods("POST")
 
 	fmt.Println("Web server listening...")
 	http.ListenAndServe(":8080", router)
