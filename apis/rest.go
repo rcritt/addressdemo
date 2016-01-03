784eae8dfa95ff5d31c700a693c341f936e5e664
rcritt <rwag@rwag.net>
2016-01-03 14:00:49 -0500

    Create is working.

--- apis/rest.go
+++ apis/rest.go
@@ -1,1 +1,19 @@
 package apis
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/rcritt/addressdemo/orm"
+	"os"
+)
+
+// Keep this open as we will keep needing it.
+var db, _ = gorm.Open("mysql", os.Getenv("DB_URL"))
+
+func init() {
+	db.DB()
+}
+
+func Create(newAddressInfo orm.AddressInfo) {
+	db.Create(&newAddressInfo)
+}
--- webserver/main.go
+++ webserver/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/rcritt/addressdemo/apis"
+	"github.com/rcritt/addressdemo/orm"
+	"io"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -30,9 +35,29 @@ func main() {
 	// TODO: https://github.com/corylanou/tns-restful-json-api/tree/master/v9
 	//       Nice logging and clean organization...
 	restAPICreate := func(w http.ResponseWriter, r *http.Request) {
+		var addressInfo orm.AddressInfo
+
 		// Read in the posted JSON request.
+		body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+		if err != nil {
+			panic(err)
+		}
+		if err := r.Body.Close(); err != nil {
+			panic(err)
+		}
+
 		// Generate a map from that.
+		if err := json.Unmarshal(body, &addressInfo); err != nil {
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(422) // unprocessable entity
+			if err := json.NewEncoder(w).Encode(err); err != nil {
+				panic(err)
+			}
+		}
+
+		fmt.Println("Here in create" + string(body))
 		// Use the information to create a new address.
+		apis.Create(addressInfo)
 
 	}
 	router.HandleFunc("/api/create", restAPICreate).Methods("POST")
